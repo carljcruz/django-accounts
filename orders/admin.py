70bdb9912bf00734167162943b89dbebc81b59f3
Carl <cruzthecarl10@gmail.com>
2020-02-19 09:55:37 +0800

    Added styling to some

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from orders.models import Order
 
-admin.site.register(Order)
 
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('date_created',)
+    list_display = ['order_number', 'customer_name', 'date_created', 'user']
+    fields = ('order_number', 'customer_name', 'date_created', 'user')
+
+admin.site.register(Order, OrderAdmin)
+
